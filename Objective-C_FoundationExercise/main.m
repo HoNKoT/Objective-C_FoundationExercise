1aba102167e22be919bde76e5a4f253e503fbb40
Hiroki.H <hon87kot12.xp@gmail.com>
2017-04-24 15:51:42 -0700

    initialize objective C project

---
+++ Objective-C_FoundationExercise/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Objective-C_FoundationExercise
+//
+//  Created by 本田裕規 on 2017-04-24.
+//  Copyright © 2017 本田裕規. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
